f6face21d203976b9915b852c6ddfc602c12264a
Sang Yoon Kim <richard13431343@gmail.com>
2019-09-07 20:07:11 +0900

    $changeprofile -> $

--- changeprofile_check.php
+++ changeprofile_check.php
@@ -21,7 +21,7 @@
         echo "<button onclick-\"location.href='changeprofile.html'\"> 돌아가기 </button>";
         exit();
     }
-    $changeprofile=mysqli_query($mysqli,"UPDATE user_info SET id = id, pw = pw WHERE name = name");
+    $changeprofile=mysqli_query($mysqli,"UPDATE user_info SET id = $id, pw = $pw WHERE name = $name");
     if($changeprofile)
     {
         ?>
