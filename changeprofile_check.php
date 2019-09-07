fe3eec72437917140cb664f5369fd17067f9bf4b
Sang Yoon Kim <richard13431343@gmail.com>
2019-09-07 20:38:55 +0900

    $changeprofile '' fix

--- changeprofile_check.php
+++ changeprofile_check.php
@@ -14,15 +14,13 @@
         echo "<button onclick=\"location.href='changeprofile.html'\"> 돌아가기 </button>";
         exit();
     }
-    //db.php 수정 꼭 필요!!
-    $changeprofile=mysqli_query($mysqli,"UPDATE user_info SET id = $id, pw = $pw WHERE name = $name");
+
+    $changeprofile=mysqli_query($mysqli,"UPDATE user_info SET id = '$id', pw = '$pw' WHERE name = '$name'");
     if($changeprofile)
     {
-        ?>
         <meta charset="utf-8" />
         <script type="text/javascript">alert('프로필 변경이 완료되었습니다.');</script>
         <meta http-equiv="refresh" content="0 url=/">
-        <?php
     }
 
     else
