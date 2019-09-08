cdde355ae1474333fc7ad499d845a488801e460f
AstroHyo <spinelee2002@gmail.com>
2019-09-08 15:37:38 +0900

    go main

--- page/board/write_ok.php
+++ page/board/write_ok.php
@@ -10,4 +10,4 @@ if(isset($_POST['lockpost'])){
 }
 $sql = mq("insert into board(name,pw,title,content,date,lock_post) values('".$_POST['name']."','".$userpw."','".$_POST['title']."','".$_POST['content']."','".$date."','".$lo_post."')"); ?>
 <script type="text/javascript">alert("글쓰기 완료되었습니다.");</script>
-<meta http-equiv="refresh" content="0 url=/" />
+<meta http-equiv="refresh" content="0 https://kmla.herokuapp.com/board_index.php" />
