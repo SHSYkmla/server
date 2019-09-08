0ce816806fd6fbe8bfa34b9c25c64cffd92624f7
AstroHyo <spinelee2002@gmail.com>
2019-09-08 22:24:54 +0900

    change

--- board_index.php
+++ board_index.php
@@ -8,6 +8,26 @@
 <link rel="stylesheet" type="text/css" href="/css/style.css" />
 </head>
 <body>
+<?php
+				session_start();
+
+				if(!isset($_SESSION['id']) || !isset($_SESSION['name']))
+				{
+					echo "<meta http-equiv='refresh' content='0;url=login.html'>";
+					exit;
+				}
+				$id = $_SESSION['id'];
+				$name = $_SESSION['name'];
+
+				echo "<p>안녕하세요, '$name'님!</p>";
+
+				//
+				//contests 여기다 추가하세요
+				//
+				echo "<a href='board_index.php' target='right'><button>게시판 가기</button></a>";
+				echo "<p><a href='changeprofile.html'>프로필 바꾸기</a></p>"; //자신의 정보를 바꾸는 새로운 코드?
+				echo "<p><a href='logout.php'>로그아웃</a></p>";
+			?>
 
 <div id="board_area">
   <h1>9인9직 for programmers in KMLA</h1>
