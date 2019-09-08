7aa578c41ff8e6ee3c30381ecb0f1cf71c43b146
AstroHyo <spinelee2002@gmail.com>
2019-09-08 22:59:47 +0900

    style

--- competition.php
+++ competition.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>순위</title>
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
 </head>
 <body>
 <div id="board_area1">
