13fc4daed5d33ed64b36e9fec2fc5b4d335372af
Sang Yoon Kim <richard13431343@gmail.com>
2019-09-08 10:17:31 +0900

    $changeprofile -> ; added

--- changeprofile_check.php
+++ changeprofile_check.php
@@ -15,14 +15,13 @@
         exit();
     }
 
-    $changeprofile=mysqli_query($mysqli,"UPDATE user_info SET id = '$id', pw = '$pw' WHERE name = '$name'");
+    $changeprofile=mysqli_query($mysqli,"UPDATE user_info SET id = '$id'; pw = '$pw' WHERE name = '$name'");
     if($changeprofile)
     {
-        <meta charset="utf-8" />
+        <meta charset="utf-8"/>
         <script type="text/javascript">alert('프로필 변경이 완료되었습니다.');</script>
         <meta http-equiv="refresh" content="0 url=/">
     }
-
     else
         echo "<button onclick=\"location.href='changeprofile.html'\"> 프로필 변경 실패, 돌아가기 </button>";
 ?>
