7cfb53d244af3fe9f7cc1c6c4be90a7eac47b740
Sang Yoon Kim <richard13431343@gmail.com>
2019-10-06 18:40:27 +0900

    deleted <php> in id and password.php

--- changeprofile_password.php
+++ changeprofile_password.php
@@ -9,7 +9,6 @@
             <body background="brownkmla.jpg">
             <h2></h2>
             <h3>회원 정보</h3>
-            <?php
             <form action="changeprofile_check.php" method="POST">
                 <p>새 비밀번호:</p>
                     <input type="number" name="new_pw">
@@ -17,7 +16,6 @@
                 <button type="submit" value="저장하기">새 프로필 저장하기</button>
 
 			</form>
-			?>
     </body>
 
 </html>
