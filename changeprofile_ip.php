08403ae334bf4113412b0cb78dcf66ac9f885620
Sang Yoon Kim <richard13431343@gmail.com>
2019-10-06 16:50:01 +0900

    id or passcode choose

---
+++ changeprofile_ip.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+
+    <body>
+        <body background="brownkmla.jpg">
+            <h2>무엇을 바꾸시겠습니까?</h2>
+            <?php
+                echo "<p><a href='changeprofile.html'>아이디 변경</br></p>";
+                echo "<p><a href='changeprofile.html'>비번 변경</br></p>";
+                echo "<p><a href='changeprofile.html'>프로필 사진 변경</a></p>";
+            ?>
+    </body>
+
+</html>
