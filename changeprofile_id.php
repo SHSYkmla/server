051ec39e5f4667fc9c3af86b5381abba093f7756
Sang Yoon Kim <richard13431343@gmail.com>
2019-10-06 18:09:53 +0900

    add php to id and password files

--- changeprofile_id.php
+++ changeprofile_id.php
@@ -9,13 +9,15 @@
             <body background="brownkmla.jpg">
             <h2></h2>
             <h3>회원 정보</h3>
+            <?php
             <form action="changeprofile_check.php" method="POST">
                 <p>새 아이디:</p>
                     <input type="text" name="new_id">
                     <br>
                 <button type="submit" value="저장하기">새 프로필 저장하기</button>
 
-			</form>
+            </form>
+            ?>
 			
     </body>
 
